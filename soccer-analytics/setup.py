cb5d280357ecc3452b15a9386fe0eb2c3b56eb01
iffthomas <thomasiff@outlook.de>
2024-04-02 10:21:31 +0200

    first commit

---
+++ soccer-analytics/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='England vs Denmark Match Analysis Project',
+    author='Group D4',
+    license='MIT',
+)
